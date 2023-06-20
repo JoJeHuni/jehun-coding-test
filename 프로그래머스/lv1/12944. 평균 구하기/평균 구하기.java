6bfa3d10706ae193ae1c27b727513cb3e87eb2ea
CHOJeHun <122279082+JoJeHuni@users.noreply.github.com>
2023-06-20 21:36:19 +0900

    [level 1] Title: 평균 구하기, Time: 0.05 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        double count = 0;
+        for (int i = 0; i<arr.length; i++) {
+            answer += arr[i];
+            count++;
+        }
+        answer = answer / count;
+        return answer;
+    }
+}
