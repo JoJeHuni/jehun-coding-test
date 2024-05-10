a58770eaa49de744c93380aa9ea2c8e961782d58
제훈 <122279082+JoJeHuni@users.noreply.github.com>
2024-05-11 00:20:37 +0900

    [level 0] Title: 소인수분해, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public List solution(int n) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                while (n % i == 0) {
+                    n = n / i;
+                }
+                answer.add(i);
+            }
+        }
+        
+        return answer;
+    }
+}
