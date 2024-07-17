a59befcfa89cd552a2723418771d88db2b51aba6
제훈 <122279082+JoJeHuni@users.noreply.github.com>
2024-07-17 20:12:38 +0900

    [Silver IV] Title: 동전 0, Time: 176 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,29 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int total = in.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String n = br.readLine();
+        String[] parts = n.split(" ");
+
+        int N = Integer.parseInt(parts[0]);
+        int total = Integer.parseInt(parts[1]);
 
         int count = 0;
-        List<Integer> list = new ArrayList<>(n);
+        List<Integer> list = new ArrayList<>(N);
 
-        for (int i = 0; i < n; i++) {
-            list.add(in.nextInt());
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.valueOf(br.readLine()));
         }
-        
+
+        StringBuilder sb = new StringBuilder();
+
         while (total > 0) {
-            if (total >= list.get(n-1)) {
-                total -= list.get(n-1);
+            if (total >= list.get(N-1)) {
+                total -= list.get(N-1);
                 count++;
             } else {
-                n--;
+                N--;
             }
             if (total == 0) break;
         }
-        System.out.println(count);
+        sb.append(count);
+        System.out.println(sb);
     }
 }
