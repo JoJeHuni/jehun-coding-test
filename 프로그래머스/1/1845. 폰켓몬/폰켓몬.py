02c1969778c0e2e786cefcd93be246db70fd5af5
제훈 <122279082+JoJeHuni@users.noreply.github.com>
2025-06-05 13:57:17 +0900

    [level 1] Title: 폰켓몬, Time: 0.74 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,5 +1,5 @@
 def solution(nums):
     answer = len(set(nums))
-    if answer > len(nums)/2:
-        return len(nums)/2
+    if (answer > len(nums) / 2) :
+        return len(nums) / 2
     return answer
