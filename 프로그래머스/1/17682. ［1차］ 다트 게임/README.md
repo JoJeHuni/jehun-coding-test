a720c0401e082c0d0fbb300c7eeed5e2e81dac6e
제훈 <122279082+JoJeHuni@users.noreply.github.com>
2025-06-17 00:26:36 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.05 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/README.md
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] [1차] 다트 게임 - 17682 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17682?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17682) 
 
 ### 성능 요약
 
-메모리: 9.49 MB, 시간: 0.04 ms
+메모리: 9.4 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 13일 16:53:47
+2025년 06월 17일 00:26:33
 
 ### 문제 설명
 
--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,9 +1,10 @@
 def solution(dartResult):
     scores = []
-    i = 0
+    num = 0
     length = len(dartResult)
+    i = 0
     
-    while i < length:
+    while i < length :
         if dartResult[i] == '1' and i + 1 < length and dartResult[i + 1] == '0':
             num = 10
             i += 2
@@ -11,24 +12,24 @@ def solution(dartResult):
             num = int(dartResult[i])
             i += 1
         
-        bonus = dartResult[i]
-        if bonus == 'S':
-            num **= 1
-        elif bonus == 'D':
-            num **= 2
-        elif bonus == 'T':
-            num **= 3
-        i += 1
-        
-        if i < length and dartResult[i] in ['*', '#']:
-            if dartResult[i] == '*':
-                num *= 2
-                if scores:
-                    scores[-1] *= 2
-            elif dartResult[i] == '#':
-                num *= -1
+        if (dartResult[i] in ['S', 'D', 'T']) :
+            if (dartResult[i] == 'S') :
+                num **= 1
+            elif (dartResult[i] == 'D') :
+                num **= 2
+            elif (dartResult[i] == 'T') :
+                num **= 3
             i += 1
         
+        if (i < length and dartResult[i] in ['*', '#']) :
+            if(dartResult[i] == '*') :
+                num *= 2
+                if (scores) :
+                    scores[-1] *= 2
+            elif(dartResult[i] == '#') :
+                num *= -1
+            i += 1
+            
         scores.append(num)
     
     return sum(scores)
